42ae467ddafd4f4a4d76290e609327031a3ed92f
JashwandAutar <jashwandautar@gmail.com>
2023-07-14 15:24:27 +0200

    Commit VUE.JS

---
+++ src/components/Customer.vue
@@ -0,0 +1,24 @@
+<script>
+    export default{
+        name: 'CustomerList'
+    }
+</script>
+
+<template>
+    <div>
+        <ul>
+            <li>Microsoft</li> 
+            <li>Google</li>
+            <li>Facebook</li> 
+            <li>Twitter</li>
+        </ul>
+    </div>
+</template>
+
+<style scoped>
+    div{
+        width: 500px;
+        border: 1px solid black;
+        text-align: center;
+    }
+</style>
